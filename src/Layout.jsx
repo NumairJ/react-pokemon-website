953c47453d095de998ad9ac2f667d345e9bb93c0
NumairJ <numair.jaan@torontomu.ca>
2025-01-03 15:44:55 -0500

    redeploy to github pages

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
       <nav>
         <ul className="nav justify-content-center">
           <li className="nav-item">
-            <Link className="nav-link" to="/">Master Pokedex</Link>
+            <Link className="nav-link" to="/Pokedex">Master Pokedex</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to="/Team-Builder">Pokemon Team Builder</Link>
